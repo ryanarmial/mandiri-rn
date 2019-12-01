69482e0c8aecde857a45878e03dadc7733ee0cdb
Ryan Akbar <ryanakbar.armial@gmail.com>
2019-12-01 10:25:49 +0700

    change to class component

--- containers/ListPeople.js
+++ containers/ListPeople.js
@@ -1,29 +1,43 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { View, Text, StyleSheet, FlatList} from 'react-native'
 import peopleList from '../utils/data.json'
 
-export default () => {
-  const [people, setPeople] = useState(peopleList)
-  console.log(people)
-  return (
-    <View style={styles.container}>
-      <Text>Daftar Nama Orang</Text>
-      <FlatList
-        data={people}
-        numColumns={3}
-        horizontal={false}
-        renderItem={({item}) => {
-          return (
-            <View style={{width: '33%'}}>
-              <Text>{item.name}</Text>
-              <Text>{item.email}</Text>
-            </View>
-          )
-        }}
-      />
-    </View>
-  )
+class ListPeople extends React.Component {
+
+  state = {
+    people: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      people: peopleList
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Daftar Nama Orang</Text>
+        <FlatList
+          data={this.state.people}
+          numColumns={3}
+          horizontal={false}
+          renderItem={({ item }) => {
+            return (
+              <View style={{ width: '33%' }}>
+                <Text>{item.name}</Text>
+                <Text>{item.email}</Text>
+              </View>
+            )
+          }}
+        />
+      </View>
+    )
+  }
+
 }
+
+export default ListPeople
 
 const styles = StyleSheet.create({
   container: {
